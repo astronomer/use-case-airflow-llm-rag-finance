307c47a94b07321d7c168d00ad4fd5a0a4abba32
TJaniF <tamara.fingerlin@astronomer.io>
2023-10-17 12:38:55 +0200

    some dag cleanup

--- dags/ingestion/in_alphavantage.py
+++ dags/ingestion/in_alphavantage.py
@@ -2,10 +2,9 @@ from datetime import datetime
 from goose3 import Goose
 import os
 import pandas as pd
-import numpy as np
 import requests
 from weaviate.util import generate_uuid5
-from weaviate_provider.hooks.weaviate import WeaviateHook
+# from weaviate_provider.hooks.weaviate import WeaviateHook
 from weaviate_provider.operators.weaviate import (
     WeaviateCheckSchemaOperator,
     WeaviateCreateSchemaOperator,
@@ -15,14 +14,19 @@ from airflow.decorators import dag, task
 from airflow.operators.empty import EmptyOperator
 
 _WEAVIATE_CONN_ID = "weaviate_test"
-IS_PRODUCTION = True
+USE_PRE_EMBEDDED_DATA = True
 
 default_args = {
     "retries": 0,
 }
 
 
-@dag(schedule=None, start_date=datetime(2023, 9, 11), catchup=False, default_args=default_args)
+@dag(
+    schedule=None,
+    start_date=datetime(2023, 9, 11),
+    catchup=False,
+    default_args=default_args,
+)
 def in_alpha_vantage():
     _check_schema = WeaviateCheckSchemaOperator(
         task_id="check_schema",
@@ -32,10 +36,7 @@ def in_alpha_vantage():
 
     @task.branch(retries=0)
     def create_schema_branch(schema_exists: bool) -> str:
-        """
-        Check if schema exists
-        """
-        WeaviateHook(_WEAVIATE_CONN_ID).get_conn().schema.delete_all()
+        # WeaviateHook(_WEAVIATE_CONN_ID).get_conn().schema.delete_all()
         if schema_exists:
             return ["schema_already_exists"]
         elif not schema_exists:
@@ -54,7 +55,7 @@ def in_alpha_vantage():
     @task(trigger_rule="none_failed")
     def get_alphavantage_news(**context):
         previous_day_market_close = "20231015T2000"  # PARAMETERIZE LATER
-        api_limit = 16 # PARAMETERIZE LATER
+        api_limit = 16  # PARAMETERIZE LATER
         ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
         url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey={ALPHAVANTAGE_API_KEY}&time_from={previous_day_market_close}&limit={api_limit}"
 
@@ -94,7 +95,6 @@ def in_alpha_vantage():
 
         return row
 
-    # CONSIDER dynamic task mapping here too!
     @task.weaviate_import(weaviate_conn_id=_WEAVIATE_CONN_ID, trigger_rule="all_done")
     def import_data(rows_with_full_texts, class_name: str):
         from transformers import BertTokenizer, BertModel
@@ -102,11 +102,16 @@ def in_alpha_vantage():
 
         news_df = pd.DataFrame(rows_with_full_texts, index=[0])
 
-        if IS_PRODUCTION:
+        if USE_PRE_EMBEDDED_DATA:
+            print("Using pre-vectorized data from parquet file.")
+            df = pd.read_parquet("include/pre_computed_embeddings/embeddings.parquet")
+        else:
             df = pd.concat([news_df], ignore_index=True)
 
             df["uuid"] = df.apply(lambda x: generate_uuid5(x.to_dict()), axis=1)
-            df["text_info"] = df.apply(lambda x: x["title"] + " " + x["summary"] + " " + x["full_text"], axis=1)
+            df["text_info"] = df.apply(
+                lambda x: x["title"] + " " + x["summary"] + " " + x["full_text"], axis=1
+            )
 
             tokenizer = BertTokenizer.from_pretrained("ProsusAI/finbert")
             model = BertModel.from_pretrained("ProsusAI/finbert")
@@ -119,7 +124,13 @@ def in_alpha_vantage():
             model.eval()
 
             def get_embeddings(texts):
-                tokens = tokenizer(texts.tolist(), return_tensors="pt", truncation=True, padding=True, max_length=512)
+                tokens = tokenizer(
+                    texts.tolist(),
+                    return_tensors="pt",
+                    truncation=True,
+                    padding=True,
+                    max_length=512,
+                )
                 with torch.no_grad():
                     outputs = model(**tokens)
                     last_hidden_state = outputs.last_hidden_state
@@ -131,16 +142,15 @@ def in_alpha_vantage():
             embeddings = []
             for i in range(0, len(df), batch_size):
                 print(f"Processing batch {i} to {i+batch_size}")
-                batch_embeddings = get_embeddings(df["text_info"].iloc[i : i + batch_size])
+                batch_embeddings = get_embeddings(
+                    df["text_info"].iloc[i : i + batch_size]
+                )
                 embeddings.extend(batch_embeddings)
 
             df["vector"] = embeddings
 
             print(f"Passing {len(df)} objects for import.")
             df.to_parquet(f"include/embeddings/embeddings_{df['uuid']}.parquet")
-        else:
-            print("Using pre-vectorized data from parquet file.")
-            df = pd.read_parquet("include/finbert_alphavantage_embeddings.parquet")
 
         return {
             "data": df,
@@ -157,7 +167,10 @@ def in_alpha_vantage():
     import_data.partial(class_name="NEWS").expand(rows_with_full_texts=full_texts)
 
     chain(
-        _check_schema, create_schema_branch(_check_schema.output), [schema_already_exists, create_schema], fetched_news
+        _check_schema,
+        create_schema_branch(_check_schema.output),
+        [schema_already_exists, create_schema],
+        fetched_news,
     )
 
 
